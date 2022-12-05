86e170a4eec929137000ec26c5069c41c785cf7a
quangdanhd <quangdanhd@gmail.com>
2022-12-05 15:55:48 +0700

    add bootstrap & tesseract

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <button
+    class="btn btn-primary"
+    data-bs-target="#collapseTarget"
+    data-bs-toggle="collapse"
+  >
+    Bootstrap collapse
+  </button>
+  <div class="collapse py-2" id="collapseTarget">
+    This is the toggle-able content!
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import Tesseract from 'tesseract.js';
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: {},
+  created() {
+    // Tesseract.recognize(
+    //     'https://tesseract.projectnaptha.com/img/eng_bw.png',
+    //     'eng',
+    //     { logger: m => console.log(m) }
+    // ).then(({ data: { text } }) => {
+    //   console.log(text);
+    // })
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+import "./assets/scss/app.scss";
+
 createApp(App).mount("#app");
